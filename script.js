fae0d845bc3033f6bf34e8b09537515ebb59a780
Abdi Dabala <abdid.yadata@gmail.com>
2025-09-21 04:06:15 -0700

    script.js

--- script.js
+++ script.js
@@ -43,3 +43,17 @@ const observer = new IntersectionObserver((entries)=>{
   });
 },{ threshold:0.2 });
 sections.forEach(section=>observer.observe(section));
+
+
+// Visitor counter using CountAPI
+fetch('https://api.countapi.xyz/hit/abdi-portfolio-website/views')
+  .then(res => res.json())
+  .then(res => {
+    // Make sure the element exists
+    const counterElement = document.getElementById("visitor-count");
+    if (counterElement) counterElement.innerText = res.value;
+  })
+  .catch(err => console.error('Error fetching visitor count:', err));
+
+
+
